c94dd93041dc8be024b7bb440a51700375206201
Prelang Builder <builder@prelang.com>
2017-01-06 18:54:15 +0000

    Defined validation for attribute 'title_image' on model 'Article'

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,5 @@ class Article < ActiveRecord::Base
   has_one :content
   validates_formatting_of :main_color, using: :hex_color
   validates_formatting_of :background_color, using: :hex_color
+  validates_formatting_of :title_image, using: :url
 end
