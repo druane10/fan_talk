95442040ba3dd1812b979581d753c72bf4d106db
Prelang Builder <builder@prelang.com>
2017-01-06 18:54:15 +0000

    Generated model Article with migration

---
+++ db/migrate/20170106185415_create_articles.rb
@@ -0,0 +1,15 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :main_color
+      t.string :background_color
+      t.string :title_image
+      t.references :user, index: true
+      t.references :category, index: true
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
