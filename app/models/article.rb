357171ffc446331a0a323317693400cf2318cb70
Prelang Builder <builder@prelang.com>
2017-01-06 18:54:15 +0000

    Defined validation for attribute 'background_color' on model 'Article'

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ActiveRecord::Base
   belongs_to :category
   has_one :content
   validates_formatting_of :main_color, using: :hex_color
+  validates_formatting_of :background_color, using: :hex_color
 end
